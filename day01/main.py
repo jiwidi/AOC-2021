3df5fbf25d7f24c384bf8c96213afe9277336e6b
jiwidi <fhjaime96@gmail.com>
2021-12-01 14:27:06 +0100

    day 01

--- day01/main.py
+++ day01/main.py
@@ -3,16 +3,18 @@ def read_input(path):
 
 
 def find_increases(sonar_output):
-    increases = 0
-    for u in range(1, len(sonar_output)):
-        if sonar_output[u - 1] < sonar_output[u]:
-            increases += 1
-    return increases
+    return sum(
+        [
+            1 if sonar_output[u - 1] < sonar_output[u] else 0
+            for u in range(1, len(sonar_output))
+        ]
+    )
 
 
 def find_increases_windowed(sonar_output):
     # We create the windows
     windows = []
+
     for u in range(len(sonar_output) - 2):
         window = sonar_output[u] + sonar_output[u + 1] + sonar_output[u + 2]
         windows.append(window)
